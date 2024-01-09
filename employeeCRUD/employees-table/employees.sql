87ea4fc072f048eb5012777af59f147b678cdf14
Nguyen Quang Anh <142569995+quanganh221@users.noreply.github.com>
2024-01-09 20:02:17 +0700

    Add files via upload

---
+++ employeeCRUD/employees-table/employees.sql
@@ -0,0 +1,20 @@
+--
+-- Table structure for table `employees`
+--
+
+CREATE TABLE IF NOT EXISTS `employees` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(100) NOT NULL,
+    `address` VARCHAR(255) NOT NULL,
+    `salary` INT(10) NOT NULL,
+    PRIMARY KEY(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=4;
+
+--
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employees` (`id`, `name`, `address`, `salary`) VALUES
+(1, 'Roland Mendel', 'C/ Araquil, 67, Madrid', 5000),
+(2, 'Victoria', '35 King George, London', 6500),
+(3, 'Martin Blank', '25, Rue Lauriston, Paris', 8000);
