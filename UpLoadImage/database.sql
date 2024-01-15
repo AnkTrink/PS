cc18ad0393dcb3e48ed44a77b52fdda38aeb9f90
Nguyen Quang Anh <142569995+quanganh221@users.noreply.github.com>
2024-01-15 09:16:07 +0700

    Add files via upload

---
+++ UpLoadImage/database.sql
@@ -0,0 +1,7 @@
+CREATE TABLE gallery (
+    idGallery INT(11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    titleGallery LONGTEXT NOT NULL,
+    descGallery LONGTEXT NOT NULL,
+    imgFullNameGallery LONGTEXT NOT NULL,
+    orderGallery INT(11) NOT NULL
+);
